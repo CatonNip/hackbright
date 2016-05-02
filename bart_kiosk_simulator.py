f95de1d25d0acb074eba5d84e4b56345e7a1cace
Vivi Nguyen <Vivi Nguyen>
2016-05-02 16:31:01 -0700

    added bart

---
+++ bart_kiosk_simulator.py
@@ -0,0 +1,10 @@
+def load_card(one,five,ten,twenty):
+    total = (one*1) + (five*5) + (ten*10) + (twenty*20)
+    
+    print(total)
+    return
+
+load_card(0, 0, 0, 0)
+load_card(0, 0, 0, 9)
+load_card(2, 3, 0, 0)
+load_card(3, 1, 1, 3)
